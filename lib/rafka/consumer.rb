9fe4c08592aaccbc97184ed949154c4d5f10a06a
Agis Anastasopoulos <agis.anast@gmail.com>
2017-07-12 12:09:26 +0300

    consumer: defer SETNAME until it's needed
    This is to keep the same behavior with redis-rb versions 3.2.2 and later.

--- lib/rafka/consumer.rb
+++ lib/rafka/consumer.rb
@@ -21,14 +21,6 @@ module Rafka
     #   underlying Redis client
     def initialize(opts={})
       @redis = Redis.new(parse_opts(opts))
-
-      # in 3.2.2 and later this is done automatically
-      #
-      # @see https://github.com/redis/redis-rb/issues/510
-      if Gem::Version.new(Redis::VERSION) < Gem::Version.new("3.2.2")
-        @redis.client.call([:client, :setname, @redis.id])
-      end
-
       @topic = "topics:#{opts[:topic]}"
     end
 
@@ -43,6 +35,14 @@ module Rafka
     # @example
     #   consume(5) { |msg| puts "I received #{msg.value}" }
     def consume(timeout=5)
+      # in 3.2.2 and later this is done automatically
+      #
+      # @see https://github.com/redis/redis-rb/issues/510
+      if !@connected && Gem::Version.new(Redis::VERSION) < Gem::Version.new("3.2.2")
+        @redis.client.call([:client, :setname, @redis.id])
+        @connected = true
+      end
+
       raised = false
       msg = nil
 
